672ae3842cf2908655148d7456ab1dcdd0ca1044
Gramsci <gramsciHC@gmail.com>
2022-03-27 23:16:30 -0400

    several updates
    * Removed scripts folder since that not was ignored by gitignore
    * implemented data generator to generate dummy data that would be available for the API
    * added two json files as example of dummy file

--- data_generator.py
+++ data_generator.py
@@ -1,13 +1,57 @@
 from datetime import datetime
 import random
 import datetime
+import optparse
+import json
 
-gender = ['Female', "Male"]
-covid_tests = random.choice(range(200, 200000))
-# confirm = random.choice(range())
-# negative = random.choice(range())
-# vaccines = random.choice(range())
+FILE_PATH = "src/%s"
 
-for i in range(0, 365):
-    # I need to add the auto generator here
-    print((datetime.date.today() - datetime.timedelta(i)).isoformat())
+
+def gender(): return random.choice(['Female', "Male"])
+def age(): return random.choice(range(1, 100))
+def covid_tests(): return random.choice(range(200, 200000))
+def confirm(): return random.choice(range(120, 10000))
+def negative(): return random.choice(range(100, 10000))
+def vaccines(): return random.choice(range(100, 500))
+
+
+def generate_dummy_data(days=365):
+    data = []
+    for index in range(0, days):
+        generated_data = {}
+        date = (datetime.date.today() - datetime.timedelta(index)).isoformat()
+        generated_data["id"] = index
+        generated_data["date"] = date
+        generated_data["gender"] = gender()
+        generated_data["age"] = age()
+        generated_data["tests"] = covid_tests()
+        generated_data["confirm"] = confirm()
+        generated_data["negative"] = negative()
+        generated_data["vaccines"] = vaccines()
+        data.append(generated_data)
+    return data
+
+
+def save_dummy_data(file_name, dummy_data):
+    file_name = "{}.json".format(
+        file_name) if "json" not in file_name else file_name
+    file = open(FILE_PATH % file_name, "a")
+    json.dump(dummy_data, file, indent=4)
+    file.close()
+
+
+parser = optparse.OptionParser()
+parser.add_option("-f", "--file", dest="file_name",
+                  help="File that would be created with the dummy data")
+parser.add_option("-d", "--days", dest="days",
+                  help="Number of days for what would be generated the dummy data")
+
+(options, arguments) = parser.parse_args()
+
+try:
+    days = int(options.days) if options.days else 365
+except Exception as ex:
+    print("Days parameter its invalid, please enter and valid integer")
+
+data = generate_dummy_data(days)
+save_dummy_data(options.file_name, data)
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+import os
 
+DATA_PATH = "src/%s.json"
+HOSPITAL_NAMES = ["hells_pass_hospital", "univalle"]
 app = Flask(__name__)
 
-hells_hospital_data = {
-    "date": ""
-}
+
+@app.get("/hospitals")
+def get_hospitals_name():
+    files = os.listdir("src/")
+    files_name = [file.split('.')[0] for file in files]
+    print(files_name)
+    return jsonify(files_name), 200
 
 
-@app.get("/hellsPassHospital")
-def get_hells_pass_hospital_data():
-    pass
+@app.get("/hospital/<hospital_name>")
+def get_hospital_data(hospital_name):
+    file_path = DATA_PATH % hospital_name
+    with open(file_path) as hells_data:
+        data = json.load(hells_data)
+        return jsonify(data), 200
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
